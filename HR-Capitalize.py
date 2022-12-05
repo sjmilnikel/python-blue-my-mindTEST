2a568181b958c07fb17a00dc89129891ccf41070
Sara Charron-Milnikel <sjmilnikel@yahoo.com>
2022-12-05 01:04:06 +0000

    HRpush

---
+++ HR-Capitalize.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s): 
+    S = "hello world"
+    s = s.title()
+    return(s)
+
+if __name__ == '__main__':
