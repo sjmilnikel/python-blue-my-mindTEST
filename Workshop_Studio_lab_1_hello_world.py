ebbf36b0018be2d0499d0cc9044d25f20853abac
Sara Charron-Milnikel <sjmilnikel@yahoo.com>
2022-12-13 03:47:18 +0000

    Lab#1

---
+++ Workshop_Studio_lab_1_hello_world.py
@@ -0,0 +1,17 @@
+# A function that prints hello world
+def hello_world():
+    print('hello world')
+
+# This line calls (runs) the function
+hello_world()
+
+
+
+
+# A function that returns hello world
+def hello_world():
+    return 'hello world'
+
+# This line calls (runs) the function
+greeting = hello_world()
+print(greeting)
