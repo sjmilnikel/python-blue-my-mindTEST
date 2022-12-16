593234874eb40517bf4de0c6d29f48f40c8e6e8f
Sara Charron-Milnikel <sjmilnikel@yahoo.com>
2022-12-16 03:02:55 +0000

    Terminate multiple EC2 instances boto3

---
+++ Python_AWS_Terminate_multiple_EC2_Instances.py
@@ -0,0 +1,13 @@
+# terminate multiple EC2 instances using boto3 python
+
+import boto3
+ec2_client=boto3.client("ec2")
+x=ec2_client.describe_instances()
+data=x["Reservations"]
+li=[]
+for instances in data:
+    instance=instances["Instances"]
+    for ids in instance:
+        instance_id=ids["InstanceId"]
+        li.append(instance_id)
+ec2_client.terminate_instances(InstanceIds=li)
